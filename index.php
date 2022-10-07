ab29b2a67ff8feb25273a7c2b2c7f17cd809b4e3
norwegian-octopus <orekhovsky.aa@gmail.com>
2022-10-07 17:30:44 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="I'm beginner at webdev.">
+    <meta property="og:url" content="https://orekhowski.ru/">
+    <meta property="og:site_name" content="Alex Orekhowski">
+    <meta property="og:title" content="Alex Orekhowski">
+    <meta property="og:description" content="I'm beginner at webdev.">
+    <meta property="twitter:domain" content="orekhowski.ru">
+    <meta property="twitter:title" content="Alex Orekhowski">
+    <meta property="twitter:description" content="I'm beginner at webdev.">
+    <meta property="twitter:url" content="https://orekhowski.ru/">
+    <!-- <link rel="sitemap" href="https://orekhowski.ru/sitemap.xml"> -->
+    <link rel="stylesheet" href="/css/style.css">
+    <script> </script>
+    <title>Alexander Orekhowski</title>
+</head>
+
+<body>
+    <div class="wrapper">
+        <header class="header">
+            <nav class="header-nav flex">
+                <a href="/" class="link">Main</a>
+                <a href="/examples" class="link">My cases</a>
+            </nav>
+            <hr class="hr-border">
+        </header>
+        <div class="greeting">
+            <h1>Hey, I'm Alexander Orekhowski</h1>
+            <p>Recently I started my journey through the webdev,</p>
+            <p>and below the tech stack I use (it will be updated):</p>
+        </div>
+        <div class="skills grid">
+            <div class="skill">
+                <a class="php" href=""></a>
+            </div>
+            <div class="skill">
+                <a class="javascript" href=""></a>
+            </div>
+            <div class="skill">
+                <a class="threejs" href=""><span>Three.js</span></a>
+            </div>
+            <div class="skill">
+                <a class="html" href=""></a>
+            </div>
+            <div class="skill">
+                <a class="css" href=""></a>
+            </div>
+            <div class="skill">
+                <a class="laravel" href=""><span>Laravel</span></a>
+            </div>
+            <div class="skill">
+                <a class="mysql" href=""></a>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
